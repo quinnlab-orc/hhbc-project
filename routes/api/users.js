a00f493e1531f2630a1bf099e88cb7ec6b96be54
Quinn <whyquinn@gmail.com>
2020-12-27 14:54:32 -0600

    account editing functionality added, might have some weird edge case issues but overall working well

--- routes/api/users.js
+++ routes/api/users.js
@@ -44,7 +44,30 @@ router.post(
   }
 );
 
+router.post("/editUser", async function (req, res) {
+  //edits existing user information
+  try {
+    const findUser = await UserModel.findOne({
+      _id: req.body._id,
+    });
+    const editedUser = {
+      // _id: req.body._id,
+      username: req.body.email,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: findUser.password,
+    };
+    res.send("user edited")
+
+    await UserModel.findOneAndUpdate({ _id: req.body._id }, editedUser)
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
 router.get("/getUser", rejectUnauthenticated, async (req, res) => {
+  //finds if a user is in the database, returns userdata if so
   try {
     const findUser = await UserModel.findOne({ email: req.user.email });
     res.send(findUser);
@@ -63,13 +86,14 @@ router.get("/logout", function (req, res) {
 });
 
 router.get("/check", rejectUnauthenticated, function (req, res) {
-  // console.log(req.user);
+  // checks if a user is authenticated, if so returns user data;
   if (req.user) {
     res.send(req.user);
   }
 });
 
 router.post("/useralbum", rejectUnauthenticated, async function (req, res) {
+  // removes existing user albums, and makes a new one
   try {
     await UserAlbum.findOneAndRemove();
     console.log(req.body);
@@ -86,6 +110,7 @@ router.post("/useralbum", rejectUnauthenticated, async function (req, res) {
 });
 
 router.get("/getUserAlbum", async function (req, res) {
+  // gets user albums from db
   try {
     const findUserAlbum = await UserAlbum.findOne();
     res.send(findUserAlbum);
--- src/components/account.js
+++ src/components/account.js
@@ -3,19 +3,53 @@ const axios = require("axios");
 
 const Account = (props) => {
   const [profileInfo, setProfileInfo] = useState({});
+  const [isEdit, setIsEdit] = useState(false);
 
-  console.log(props.onLoggedIn);
   useEffect(() => {
     if (props.onLoggedIn.firstname) {
       setProfileInfo(props.onLoggedIn);
     }
   }, []);
 
+  useEffect(() => {
+    axios
+      .get("/api/users/getUser")
+      .then(function (response) {
+        setProfileInfo(response.data)
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+
   const logOut = () => {
     axios
       .get("http://localhost:5000/api/users/logout")
+      .then(function (response) {})
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+
+  const editAccount = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const [editedAccount, setEditedAccount] = useState({
+    _id: props.onLoggedIn._id,
+    firstname: props.onLoggedIn.firstname,
+    lastname: props.onLoggedIn.lastname,
+    email: props.onLoggedIn.email,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/users/editUser", editedAccount)
       .then(function (response) {
         console.log(response);
+        setProfileInfo(editedAccount);
+        setIsEdit(!isEdit);
       })
       .catch(function (error) {
         console.error(error);
@@ -24,14 +58,75 @@ const Account = (props) => {
 
   return (
     <div>
-      <div className="profile">
-        <span className="profileinfo">Name: {profileInfo.firstname}</span>
-        <span className="profileinfo">Last: {profileInfo.lastname}</span>
-        <span className="profileinfo">Email: {profileInfo.email}</span>
+      <div>
+        <form onSubmit={() => logOut()}>
+          <button type="submit">Log Out</button>
+        </form>
+        <button onClick={() => editAccount()}>Edit</button>
+
+        {isEdit === true ? (
+          <div>
+            <div className="profile">
+              <span className="profileinfo">Name: {profileInfo.firstname}</span>
+              <span className="profileinfo">Last: {profileInfo.lastname}</span>
+              <span className="profileinfo">Email: {profileInfo.email}</span>
+            </div>
+            <div>
+              <form onSubmit={(e) => handleSubmit(e)} className="profileEdit">
+                <input
+                  className="editInput"
+                  placeholder="Name"
+                  defaultValue={profileInfo.firstname}
+                  onChange={(event) => {
+                    setEditedAccount({
+                      _id: props.onLoggedIn._id,
+                      firstname: event.target.value,
+                      lastname: editedAccount.lastname,
+                      email: editedAccount.email,
+                    });
+                  }}
+                ></input>
+                <input
+                  className="editInput"
+                  placeholder="Last"
+                  defaultValue={profileInfo.lastname}
+                  onChange={(event) => {
+                    setEditedAccount({
+                      _id: props.onLoggedIn._id,
+                      firstname: editedAccount.firstname,
+                      lastname: event.target.value,
+                      email: editedAccount.email,
+                    });
+                  }}
+                ></input>
+                <input
+                  className="editInput"
+                  placeholder="Email"
+                  defaultValue={profileInfo.email}
+                  type="email"
+                  onChange={(event) => {
+                    setEditedAccount({
+                      _id: props.onLoggedIn._id,
+                      firstname: editedAccount.firstname,
+                      lastname: editedAccount.lastname,
+                      email: event.target.value,
+                    });
+                  }}
+                ></input>
+                <button type="submit" className="editInputButton">
+                  Submit
+                </button>
+              </form>
+            </div>
+          </div>
+        ) : (
+          <div className="profile">
+            <span className="profileinfo">Name: {profileInfo.firstname}</span>
+            <span className="profileinfo">Last: {profileInfo.lastname}</span>
+            <span className="profileinfo">Email: {profileInfo.email}</span>
+          </div>
+        )}
       </div>
-      <form>
-        <button onClick={() => logOut()}>Log Out</button>
-      </form>
     </div>
   );
 };
